ea712b6646f1e4cf42330e15eb08fdd89bfb1a70
Gabo64277 <gabriel.grabovac24@gmail.com>
2025-04-23 11:55:11 +0200

    Add files via upload

---
+++ zadaci_funkcije/5.py
@@ -0,0 +1,10 @@
+def izracunaj_prosjek(lista):
+    zbroj=0
+    for element in lista:
+        zbroj+=element
+    if len(lista) > 0:
+        return zbroj / len(lista)
+    else:
+        return 0
+print(izracunaj_prosjek([1, 2, 3, 4, 5]))
+print(izracunaj_prosjek([])) 
